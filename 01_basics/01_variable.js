7306b9375f55cb1d88d80d8680f32baae334a78b
Rupam18 <138404984+Rupam18@users.noreply.github.com>
2023-12-14 11:05:35 +0000

    datatpes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accId = 33344
+let accemail= "rupam@google.com"
+var accpw  = "acc123"
+acccity = "bhubaneswar"
+/*
+do not use var bacuse of issue in block scope and future scope;
+always use let const while writing code in javascript */
+accemail="goat@hc.com"
+accpw ="324321"
+acccity = "sng"
+let accstate
+
+console.log(accId);
+console.table([accId,accemail,accpw,acccity,accstate])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict";
+console.log(3+3);
+console.log("rupam"); //code readability should be high
+//use ecma script to know how to write standard code in js
+
+//number => 2 to power of 53
+//big int
+//string="rupam"
+//boolean= true/false
+//null= standalone value
+//undefined
+//symbol = unique
+//object 
+
+console.log(typeof null); //object (special case)
+console.log(typeof undefined);// undefined
