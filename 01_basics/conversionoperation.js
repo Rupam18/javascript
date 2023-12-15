37f1323aab53c530ea452787102f1c3c2a5a71bf
Rupam18 <138404984+Rupam18@users.noreply.github.com>
2023-12-15 11:15:12 +0000

    conversion opeartions

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,24 @@
+let score = true
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//33 = 33
+//33abc = NaN(not a number)
+//true =1 ; false =0;
+
+let isloggedin = ""
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin);
+//1 = true ;0= false
+//""= false
+//"rupam"= true
+ 
+let somenumber = 33
+
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
