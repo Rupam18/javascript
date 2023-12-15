49d1198a548af759ab6bc49b7a7fb5ebd8f8bb02
Rupam18 <138404984+Rupam18@users.noreply.github.com>
2023-12-15 12:35:48 +0000

    comparisions

---
+++ 01_basics/conversionopr.js
@@ -0,0 +1,22 @@
+let value =3 
+let negvalue = -value
+console.log(negvalue);
+
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/2);
+//console.log(2%2);
+
+let str1 = "hello"
+let str2 = " rupam"
+let str3 = str1 + str2;
+console.log(str3);
+
+let gamecounter =100
+++gamecounter;
+console.log(gamecounter);
+
+//mdn webdocs for incremental and decremental values code and explanation
+
