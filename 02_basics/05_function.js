7a1f4b567e220245190a973acbc466e42e876c4d
Rupam Kumar Sarangi <138404984+Rupam18@users.noreply.github.com>
2023-12-20 14:55:35 +0000

    function

---
+++ 02_basics/05_function.js
@@ -0,0 +1,33 @@
+function saymyname () {
+    console.log("R");
+    console.log("U");
+    console.log("P");
+    console.log("A");
+    console.log("M");
+}
+//saymyname()
+
+//function  addtwonumber (number1, number2) {
+//    console.log(number1+ number2);
+//}
+
+//addtwonumber(3,5)
+function  addtwonumber (number1, number2) {
+    //let result =number1 + number2
+    //return result
+}
+const result = addtwonumber(3,5)
+
+//console.log("result :", result);
+
+function loginusermessage (username){
+ if (username === undefined){
+    console.log("please enter a username ");
+    return
+ }
+ else
+ return username + " just logged in"
+}
+
+//console.log(loginusermessage("rupam"));
+console.log(loginusermessage("RUPAM"))
